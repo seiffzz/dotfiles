cf8ab8854b142b3c7fb29961764d3c7b301152ad
seiffzz <seifalaa@143@gmail.com>
2024-09-09 19:56:31 +0300

    add zed config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 -- COLOR SHCEME
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1
 config.macos_window_background_blur = 10
 config.window_background_image = "/Users/seiffzz/.config/wallpapers/bg-blurred-darker.png"
 
